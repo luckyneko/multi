d97ea683146bc95886b191f689aece308ef0efd9
LuckyNeko <feik@luckyneko.com>
2020-03-15 14:42:41 +1300

    add basic thread safe queue.

--- test/main.cpp
+++ test/main.cpp
@@ -1,2 +1,25 @@
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
+
+#include "details/queue.h"
+
+TEST_CASE( "Test Queue", "[queue]" )
+{
+	multi::Queue<int> testQueue;
+	int out = 0;
+	CHECK(testQueue.pop(&out) == false);
+
+	testQueue.push(7);
+	CHECK(testQueue.empty() == false);
+	CHECK(testQueue.pop(&out) == true);
+	CHECK(out == 7);
+	CHECK(testQueue.empty() == true);
+
+	testQueue.emplace(9);
+	CHECK(testQueue.empty() == false);
+	CHECK(testQueue.pop(&out) == true);
+	CHECK(out == 9);
+	CHECK(testQueue.empty() == true);
+
+	CHECK(testQueue.pop(&out) == false);
+}
