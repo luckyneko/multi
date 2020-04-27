b96cbda22f9da7224276b305694c47ab3bc9139c
LuckyNeko <feik@luckyneko.com>
2020-04-28 07:27:33 +1200

    fix addcatch2 to support changing version.Bump to v2.12.1

--- cmake/addcatch2.cmake
+++ cmake/addcatch2.cmake
@@ -6,24 +6,24 @@ find_package(Catch2 CONFIG)
 
 if (${Catch2_FOUND})       
 else ()
-	set(CATCH2_VER "2.11.1")
-	if(NOT EXISTS "${CMAKE_SOURCE_DIR}/thirdparty/catch2.tar.gz")
-		message(STATUS "Downloading Catch2")
+	set(CATCH2_VER "2.12.1")
+	if(NOT EXISTS "${CMAKE_SOURCE_DIR}/thirdparty/Catch2-${CATCH2_VER}.tar.gz")
+		message(STATUS "Downloading Catch2 (${CATCH2_VER})")
 		file(DOWNLOAD 
 			"https://github.com/catchorg/Catch2/archive/v${CATCH2_VER}.tar.gz" 
-			"${CMAKE_SOURCE_DIR}/thirdparty/catch2.tar.gz"
+			"${CMAKE_SOURCE_DIR}/thirdparty/Catch2-${CATCH2_VER}.tar.gz"
 		)
 	endif()
 
 	if(NOT EXISTS "${CMAKE_SOURCE_DIR}/thirdparty/Catch2-${CATCH2_VER}")
-		message(STATUS "Decompress Catch2")
+		message(STATUS "Decompress Catch2 (${CATCH2_VER})")
 		execute_process(COMMAND 
-			${CMAKE_COMMAND} -E tar xfz "${CMAKE_SOURCE_DIR}/thirdparty/catch2.tar.gz"
+			${CMAKE_COMMAND} -E tar xfz "${CMAKE_SOURCE_DIR}/thirdparty/Catch2-${CATCH2_VER}.tar.gz"
 			WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}/thirdparty"
 		)
 	endif()
 
-	message(STATUS "Include Catch2")
+	message(STATUS "Using thirdparty/Catch2 (${CATCH2_VER})")
 	add_subdirectory(${CMAKE_SOURCE_DIR}/thirdparty/Catch2-${CATCH2_VER})
 	list(APPEND CMAKE_MODULE_PATH "${CMAKE_SOURCE_DIR}/thirdparty/Catch2-${CATCH2_VER}/contrib")
 endif ()
