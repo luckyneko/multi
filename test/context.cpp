70fbec3127b8500f4147c72a84df98e5b5070a75
Feik <feik@luckyneko.com>
2020-09-23 17:09:38 +1200

    add tests for 0, 1, 2 & 4 threadcounts

--- test/context.cpp
+++ test/context.cpp
@@ -9,40 +9,47 @@
 #include <catch2/catch.hpp>
 #include <multi/context.h>
 
+#include <array>
+
 TEST_CASE("multi::Context")
 {
 	multi::Context context;
 	REQUIRE(context.threadCount() == 0);
 
-	// Start threads
-	context.start(4);
-	REQUIRE(context.threadCount() == 4);
+	std::array<size_t, 4> threadCountSet = {0, 1, 2, 4};
+	for (auto threadCount : threadCountSet)
+	{
+		// Start threads
+		context.start(threadCount);
+		REQUIRE(context.threadCount() == threadCount);
 
-	// Test handle wait
-	std::atomic<int> a(0);
-	auto hdl = context.async([&](multi::Job& jb) {
-		std::this_thread::sleep_for(std::chrono::milliseconds(1));
-		++a;
-	});
-	CHECK(a == 0);
-	hdl.wait();
-	CHECK(a == 1);
+		// Test handle wait
+		std::atomic<int> a(0);
+		auto hdl = context.async([&](multi::Job& jb) {
+			std::this_thread::sleep_for(std::chrono::milliseconds(1));
+			++a;
+		});
+		if (threadCount > 0)
+			CHECK(a == 0);
+		hdl.wait();
+		CHECK(a == 1);
 
-	// Reset handle
-	CHECK(hdl.valid() == true);
-	CHECK(hdl.complete() == true);
-	hdl = multi::Handle();
-	CHECK(hdl.valid() == false);
-	CHECK(hdl.complete() == true);
+		// Reset handle
+		CHECK(hdl.valid() == true);
+		CHECK(hdl.complete() == true);
+		hdl = multi::Handle();
+		CHECK(hdl.valid() == false);
+		CHECK(hdl.complete() == true);
 
-	// Test drop handle
-	context.async([&](multi::Job& jb) {
-		std::this_thread::sleep_for(std::chrono::milliseconds(1));
-		++a;
-	});
-	CHECK(a == 2);
+		// Test drop handle
+		context.async([&](multi::Job& jb) {
+			std::this_thread::sleep_for(std::chrono::milliseconds(1));
+			++a;
+		});
+		CHECK(a == 2);
 
-	// Stop threads
-	context.stop();
-	REQUIRE(context.threadCount() == 0);
+		// Stop threads
+		context.stop();
+		REQUIRE(context.threadCount() == 0);
+	}
 }
--- test/job.cpp
+++ test/job.cpp
@@ -10,125 +10,146 @@
 #include <catch2/catch.hpp>
 #include <multi/multi.h>
 
+std::array<size_t, 4> threadCountSet = {0, 1, 2, 4};
+
 TEST_CASE("multi::Job::add()")
 {
 	multi::Context context;
 	REQUIRE(context.threadCount() == 0);
-	context.start(4);
-	REQUIRE(context.threadCount() == 4);
 
-	// Paralell check
-	std::atomic<int> a(0);
-	context.async([&](multi::Job& jb) {
-		++a;
-		jb.add(multi::Order::par, [&](multi::Job& jb) {
-			std::this_thread::sleep_for(std::chrono::milliseconds(1));
-			a = a.load() * 3;
+	for (auto threadCount : threadCountSet)
+	{
+		context.start(threadCount);
+		REQUIRE(context.threadCount() == threadCount);
+
+		// Parallel check
+		std::atomic<int> a(0);
+		context.async([&](multi::Job& jb) {
+			++a;
+			jb.add(multi::Order::par, [&](multi::Job& jb) {
+				std::this_thread::sleep_for(std::chrono::milliseconds(1));
+				a = a.load() * 3;
+			});
+			++a;
 		});
-		++a;
-	});
-	CHECK(a == 6);
+		if (threadCount == 0)
+			CHECK(a == 4);
+		else
+			CHECK(a == 6);
 
-	// Sequential check
-	std::atomic<int> b(0);
-	context.async([&](multi::Job& jb) {
-		jb.add(
-			multi::Order::seq, [&](multi::Job& jb) {
+		// Sequential check
+		std::atomic<int> b(0);
+		context.async([&](multi::Job& jb) {
+			jb.add(
+				multi::Order::seq, [&](multi::Job& jb) {
 			std::this_thread::sleep_for(std::chrono::milliseconds(2));
 			++b; },
-			[&](multi::Job& jb) {
-				b = b * 2;
-			},
-			[&](multi::Job& jb) {
-				++b;
-			});
-	});
-	CHECK(b == 3);
+				[&](multi::Job& jb) {
+					b = b * 2;
+				},
+				[&](multi::Job& jb) {
+					++b;
+				});
+		});
+		CHECK(b == 3);
 
-	context.stop();
-	REQUIRE(context.threadCount() == 0);
+		context.stop();
+		REQUIRE(context.threadCount() == 0);
+	}
 }
 
 TEST_CASE("multi::Job::each()")
 {
 	multi::Context context;
 	REQUIRE(context.threadCount() == 0);
-	context.start(4);
-	REQUIRE(context.threadCount() == 4);
 
-	// Parallel check
-	std::atomic<int> a(0);
-	std::array<int, 4> b = {1, 2, 3, 4};
-	context.async([&](multi::Job& jb) {
-		jb.each(multi::Order::par, b.begin(), b.end(), [&](multi::Job& jb, int i) {
-			std::this_thread::sleep_for(std::chrono::milliseconds(i));
-			a += i;
+	for (auto threadCount : threadCountSet)
+	{
+		context.start(threadCount);
+		REQUIRE(context.threadCount() == threadCount);
+
+		// Parallel check
+		std::atomic<int> a(0);
+		std::array<int, 4> b = {1, 2, 3, 4};
+		context.async([&](multi::Job& jb) {
+			jb.each(multi::Order::par, b.begin(), b.end(), [&](multi::Job& jb, int i) {
+				std::this_thread::sleep_for(std::chrono::milliseconds(i));
+				a += i;
+			});
 		});
-	});
-	CHECK(a == 10);
+		CHECK(a == 10);
 
-	// Seq check
-	int c = 1;
-	std::array<int, 4> d = {1, 2, 3, 4};
-	context.async([&](multi::Job& jb) {
-		jb.each(multi::Order::seq, d.begin(), d.end(), [&](multi::Job& jb, int i) {
-			c *= i;
+		// Seq check
+		int c = 1;
+		std::array<int, 4> d = {1, 2, 3, 4};
+		context.async([&](multi::Job& jb) {
+			jb.each(multi::Order::seq, d.begin(), d.end(), [&](multi::Job& jb, int i) {
+				c *= i;
+			});
 		});
-	});
-	CHECK(c == 24);
+		CHECK(c == 24);
 
-	context.stop();
-	REQUIRE(context.threadCount() == 0);
+		context.stop();
+		REQUIRE(context.threadCount() == 0);
+	}
 }
 
 TEST_CASE("multi::Job::range()")
 {
 	multi::Context context;
 	REQUIRE(context.threadCount() == 0);
-	context.start(4);
-	REQUIRE(context.threadCount() == 4);
 
-	// Parallel check
-	std::atomic<int> a(0);
-	std::array<int, 4> b = {1, 2, 3, 4};
-	context.async([&](multi::Job& jb) {
-		jb.range(multi::Order::par, 0, int(b.size()), 1, [&](multi::Job& jb, int i) {
-			std::this_thread::sleep_for(std::chrono::milliseconds(i));
-			a += b[i];
+	for (auto threadCount : threadCountSet)
+	{
+		context.start(threadCount);
+		REQUIRE(context.threadCount() == threadCount);
+
+		// Parallel check
+		std::atomic<int> a(0);
+		std::array<int, 4> b = {1, 2, 3, 4};
+		context.async([&](multi::Job& jb) {
+			jb.range(multi::Order::par, 0, int(b.size()), 1, [&](multi::Job& jb, int i) {
+				std::this_thread::sleep_for(std::chrono::milliseconds(i));
+				a += b[i];
+			});
 		});
-	});
-	CHECK(a == 10);
+		CHECK(a == 10);
 
-	// Seq check
-	int c = 1;
-	std::array<int, 4> d = {1, 2, 3, 4};
-	context.async([&](multi::Job& jb) {
-		jb.range(multi::Order::seq, 0, int(d.size()), 1, [&](multi::Job& jb, int i) {
-			c *= d[i];
+		// Seq check
+		int c = 1;
+		std::array<int, 4> d = {1, 2, 3, 4};
+		context.async([&](multi::Job& jb) {
+			jb.range(multi::Order::seq, 0, int(d.size()), 1, [&](multi::Job& jb, int i) {
+				c *= d[i];
+			});
 		});
-	});
-	CHECK(c == 24);
+		CHECK(c == 24);
 
-	context.stop();
-	REQUIRE(context.threadCount() == 0);
+		context.stop();
+		REQUIRE(context.threadCount() == 0);
+	}
 }
 
 TEST_CASE("multi::Job::until()")
 {
 	multi::Context context;
 	REQUIRE(context.threadCount() == 0);
-	context.start(4);
-	REQUIRE(context.threadCount() == 4);
 
-	// Seq check
-	int c = 1;
-	int i = 0;
-	std::array<int, 4> d = {1, 2, 3, 4};
-	context.async([&](multi::Job& jb) {
-		jb.until([&]() { return c > 4 && i < d.size(); }, [&](multi::Job& jb) { c *= d[i]; ++i; });
-	});
-	CHECK(c == 6);
+	for (auto threadCount : threadCountSet)
+	{
+		context.start(threadCount);
+		REQUIRE(context.threadCount() == threadCount);
 
-	context.stop();
-	REQUIRE(context.threadCount() == 0);
+		// Seq check
+		int c = 1;
+		int i = 0;
+		std::array<int, 4> d = {1, 2, 3, 4};
+		context.async([&](multi::Job& jb) {
+			jb.until([&]() { return c > 4 && i < d.size(); }, [&](multi::Job& jb) { c *= d[i]; ++i; });
+		});
+		CHECK(c == 6);
+
+		context.stop();
+		REQUIRE(context.threadCount() == 0);
+	}
 }
