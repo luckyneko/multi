7bb2203d5f9ed94ea3c19ccda72f6cb5d4b15935
Feik <feik@luckyneko.com>
2022-02-27 10:19:47 +1300

    add unit tests & fix for each/range edge cases

--- include/multi/details/context.inl
+++ include/multi/details/context.inl
@@ -19,7 +19,7 @@ namespace multi
 	void Context::each(ITER begin, ITER end, FUNC&& func)
 	{
 		std::vector<Task> taskList;
-		taskList.reserve(end - begin);
+		taskList.reserve(std::distance(begin, end));
 		for (ITER it = begin; it != end; ++it)
 			taskList.emplace_back(std::bind(func, std::ref(*it)));
 		runQueueJob(std::move(taskList));
@@ -28,20 +28,43 @@ namespace multi
 	template <typename ITER, typename FUNC>
 	void Context::each(size_t taskCount, ITER begin, ITER end, FUNC&& func)
 	{
-		auto numLoops = (end - begin);
-		size_t outerStep = numLoops / (taskCount - 1);
+		// Handle single threaded case
+		if (taskCount <= 1)
+		{
+			for (ITER it = begin; it != end; ++it)
+				func(std::ref(*it));
+			return;
+		}
+
+		// Handle less tasks than requested taskCount case
+		auto totalTaskCount = std::distance(begin, end);
+		if (totalTaskCount <= taskCount)
+		{
+			each(begin, end, std::move(func));
+			return;
+		}
 
 		std::vector<Task> taskList;
 		taskList.reserve(taskCount);
+		ITER innerBegin = begin;
+		ITER innerEnd = innerBegin;
+		const size_t outerStep = totalTaskCount / (taskCount - 1);
 		for (size_t i = 0; i < taskCount; ++i)
 		{
-			ITER innerBegin = begin + (i * outerStep);
-			ITER innerEnd = (i + 1 == taskCount) ? end : innerBegin + outerStep;
+			// Set end
+			if (i + 1 == taskCount)
+				innerEnd = end;
+			else
+				std::advance(innerEnd, outerStep);
+
 			taskList.emplace_back([innerBegin, innerEnd, func]()
 								  {
-									  for (ITER it = innerBegin; it < innerEnd; ++it)
+									  for (ITER it = innerBegin; it != innerEnd; ++it)
 										  func(std::ref(*it));
 								  });
+
+			// Step begin
+			innerBegin = innerEnd;
 		}
 		runQueueJob(std::move(taskList));
 	}
@@ -52,6 +75,12 @@ namespace multi
 	template <typename IDX, typename FUNC>
 	void Context::range(IDX begin, IDX end, IDX step, FUNC&& func)
 	{
+		if (step == 0)
+			return;
+
+		if (end <= begin)
+			return;
+
 		std::vector<Task> taskList;
 		taskList.reserve((end - begin) / step);
 		for (IDX i = begin; i < end; i += step)
@@ -62,8 +91,29 @@ namespace multi
 	template <typename IDX, typename FUNC>
 	void Context::range(size_t taskCount, IDX begin, IDX end, IDX step, FUNC&& func)
 	{
-		IDX numLoops = (end - begin) / step;
-		IDX outerStep = numLoops / (taskCount - 1);
+		if (step == 0)
+			return;
+
+		if (end <= begin)
+			return;
+
+		// Handle single threaded case
+		if (taskCount <= 1)
+		{
+			for (IDX i = begin; i < end; i += step)
+				func(i);
+			return;
+		}
+
+		// Handle less tasks than requested taskCount case
+		size_t totalTaskCount = size_t((end - begin) / step) + (((end - begin) % step > 0) ? 1 : 0);
+		if (totalTaskCount <= taskCount)
+		{
+			range(begin, end, step, std::move(func));
+			return;
+		}
+
+		IDX outerStep = step * (totalTaskCount / (taskCount - 1));
 		range(begin, end, outerStep, [&](IDX innerBegin)
 			  {
 				  for (IDX i = innerBegin; i < innerBegin + outerStep && i < end; i += step)
--- test/context.cpp
+++ test/context.cpp
@@ -9,96 +9,226 @@
 #include <catch2/catch.hpp>
 #include <multi/context.h>
 
+#include <algorithm>
 #include <array>
+#include <map>
+
+static const std::array<size_t, 4> THREAD_COUNT_SET = {0, 1, 2, 4};
+static const std::array<size_t, 4> TASK_COUNT_SET = {0, 1, 2, 4};
+static const size_t DATA_SET_MAX_SIZE = 32;
+
+void testAsync(multi::Context& context)
+{
+	// Test handle wait
+	std::atomic<int> a(0);
+	auto hdl = context.async([&]()
+							 {
+								 std::this_thread::sleep_for(std::chrono::milliseconds(1));
+								 ++a; });
+	if (context.threadCount() > 0)
+		CHECK(a == 0);
+	hdl.wait();
+	CHECK(a == 1);
+
+	// Reset handle
+	CHECK(hdl.valid() == true);
+	CHECK(hdl.complete() == true);
+	hdl = multi::Handle();
+	CHECK(hdl.valid() == false);
+	CHECK(hdl.complete() == true);
+
+	// Test drop handle
+	a = 1;
+	context.async([&]()
+				  {
+					  std::this_thread::sleep_for(std::chrono::milliseconds(1));
+					  ++a; });
+	CHECK(a == 2);
+}
+
+void testParallel(multi::Context& context)
+{
+	// Test parallel
+	std::atomic<int> a(2);
+	context.parallel([&]()
+					 { ++a; },
+					 [&]()
+					 {
+						 std::this_thread::sleep_for(std::chrono::milliseconds(1));
+						 a = a * 2;
+					 });
+	CHECK(a == 6);
+}
+
+void testEach(multi::Context& context)
+{
+	// Each of vector
+	std::vector<int> inVec;
+	int inVecSum = 0;
+	for (size_t i = 0; i < DATA_SET_MAX_SIZE; ++i)
+	{
+		// Test mutable Each
+		std::vector<int> tmpVec = inVec;
+		context.each(tmpVec.begin(), tmpVec.end(), [](int& i)
+					 { i += 1; });
+
+		// Test const Each
+		std::atomic<int> a(0);
+		context.each(tmpVec.begin(), tmpVec.end(), [&](int i)
+					 { a += i; });
+		CHECK(a == inVecSum + i);
+
+		inVec.push_back(i);
+		inVecSum += i;
+	}
+
+	// Each of Map
+	std::map<int, int> inMap;
+	int inMapSum = 0;
+	for (size_t i = 0; i < DATA_SET_MAX_SIZE; ++i)
+	{
+		// Test mutable each map Iterator
+		std::map<int, int> tmpMap = inMap;
+		context.each(tmpMap.begin(), tmpMap.end(), [](std::pair<const int, int>& p)
+					 { p.second += 1; });
+
+		// Test each map Iterator
+		std::atomic<int> a(0);
+		context.each(tmpMap.begin(), tmpMap.end(), [&](const std::pair<const int, int>& p)
+					 { a += p.second; });
+		CHECK(a == inMapSum + i);
+
+		inMap[i] = i;
+		inMapSum += i;
+	}
+}
+
+void testEachTaskCount(multi::Context& context, size_t taskCount)
+{
+	// Each of vector
+	std::vector<int> inVec;
+	int inVecSum = 0;
+	for (size_t i = 0; i < DATA_SET_MAX_SIZE; ++i)
+	{
+		// Test mutable Each
+		std::vector<int> tmpVec = inVec;
+		context.each(taskCount, tmpVec.begin(), tmpVec.end(), [](int& i)
+					 { i += 1; });
+
+		// Test const Each
+		std::atomic<int> a(0);
+		context.each(taskCount, tmpVec.begin(), tmpVec.end(), [&](int i)
+					 { a += i; });
+		CHECK(a == inVecSum + i);
+
+		inVec.push_back(i);
+		inVecSum += i;
+	}
+
+	// Each of Map
+	std::map<int, int> inMap;
+	int inMapSum = 0;
+	for (size_t i = 0; i < DATA_SET_MAX_SIZE; ++i)
+	{
+		// Test mutable each map Iterator
+		std::map<int, int> tmpMap = inMap;
+		context.each(taskCount, tmpMap.begin(), tmpMap.end(), [](std::pair<const int, int>& p)
+					 { p.second += 1; });
+
+		// Test each map Iterator
+		std::atomic<int> a(0);
+		context.each(taskCount, tmpMap.begin(), tmpMap.end(), [&](const std::pair<const int, int>& p)
+					 { a += p.second; });
+		CHECK(a == inMapSum + i);
+
+		inMap[i] = i;
+		inMapSum += i;
+	}
+}
+
+void testRange(multi::Context& context)
+{
+	// Test int
+	for (int begin = 0; begin < DATA_SET_MAX_SIZE; ++begin)
+	{
+		for (int end = 0; end < DATA_SET_MAX_SIZE; ++end)
+		{
+			for (int step = 1; step < DATA_SET_MAX_SIZE; ++step)
+			{
+				std::atomic<int> a(0);
+				context.range(begin, end, step, [&](int i)
+							  { a += 1; });
+
+				const int totalRange = std::max(0, (end - begin));
+				const int expectedResult = (totalRange / step) + ((totalRange % step > 0) ? 1 : 0);
+				CHECK(a == expectedResult);
+			}
+		}
+	}
+
+	// Test float
+	const float scale = 0.1f;
+	for (float begin = 0; begin < scale * DATA_SET_MAX_SIZE; begin += scale)
+	{
+		for (float end = 0; end < scale * DATA_SET_MAX_SIZE; end += scale)
+		{
+			for (float step = scale; step < scale * DATA_SET_MAX_SIZE; step += scale)
+			{
+				std::atomic<int> a(0);
+				context.range(begin, end, step, [&](float i)
+							  { a += 1; });
+
+				int expectedResult = 0;
+				for (float i = begin; i < end; i += step)
+					++expectedResult;
+
+				CHECK(a == expectedResult);
+			}
+		}
+	}
+}
+
+void testRangeTaskCount(multi::Context& context, size_t taskCount)
+{
+	for (int begin = 0; begin < DATA_SET_MAX_SIZE; ++begin)
+	{
+		for (int end = 0; end < DATA_SET_MAX_SIZE; ++end)
+		{
+			for (int step = 1; step < DATA_SET_MAX_SIZE; ++step)
+			{
+				std::atomic<int> a(0);
+				context.range(taskCount, begin, end, step, [&](int i)
+							  { a += 1; });
+
+				const int totalRange = std::max(0, (end - begin));
+				const int expectedResult = (totalRange / step) + ((totalRange % step > 0) ? 1 : 0);
+				CHECK(a == expectedResult);
+			}
+		}
+	}
+}
 
 TEST_CASE("multi::Context")
 {
 	multi::Context context;
 	REQUIRE(context.threadCount() == 0);
 
-	std::array<size_t, 4> threadCountSet = {0, 1, 2, 4};
-	for (auto threadCount : threadCountSet)
+	for (auto threadCount : THREAD_COUNT_SET)
 	{
 		// Start threads
 		context.start(threadCount);
 		REQUIRE(context.threadCount() == threadCount);
 
-		// Test handle wait
-		std::atomic<int> a(0);
-		auto hdl = context.async([&]()
-								 {
-									 std::this_thread::sleep_for(std::chrono::milliseconds(1));
-									 ++a;
-								 });
-		if (threadCount > 0)
-			CHECK(a == 0);
-		hdl.wait();
-		CHECK(a == 1);
+		testAsync(context);
+		testParallel(context);
+		testEach(context);
+		testRange(context);
 
-		// Reset handle
-		CHECK(hdl.valid() == true);
-		CHECK(hdl.complete() == true);
-		hdl = multi::Handle();
-		CHECK(hdl.valid() == false);
-		CHECK(hdl.complete() == true);
-
-		// Test drop handle
-		a = 1;
-		context.async([&]()
-					  {
-						  std::this_thread::sleep_for(std::chrono::milliseconds(1));
-						  ++a;
-					  });
-		CHECK(a == 2);
-
-		// Test parallel
-		a = 2;
-		context.parallel([&]()
-						 { ++a; },
-						 [&]()
-						 {
-							 std::this_thread::sleep_for(std::chrono::milliseconds(1));
-							 a = a * 2;
-						 });
-		CHECK(a == 6);
-
-		// Test const Each
-		a = 0;
-		const std::vector<int> intList{0, 1, 2, 3, 4};
-		context.each(intList.begin(), intList.end(), [&](int i)
-					 { a += i; });
-		CHECK(a == 10);
-
-		// TEst const Each task set
-		a = 0;
-		context.each(4, intList.begin(), intList.end(), [&](int i)
-					 { a += i; });
-		CHECK(a == 10);
-
-		// Test mutable Each
-		std::vector<int> intAList = intList;
-		context.each(intAList.begin(), intAList.end(), [](int& i)
-					 { i += 1; });
-		CHECK(intAList == std::vector<int>{1, 2, 3, 4, 5});
-
-		// Test mutable Each task set
-		std::vector<int> intBList = intList;
-		context.each(4, intBList.begin(), intBList.end(), [](int& i)
-					 { i += 1; });
-		CHECK(intBList == std::vector<int>{1, 2, 3, 4, 5});
-
-		// Test Range
-		a = 0;
-		context.range(1, 15, 2, [&](int i)
-					  { a += i; });
-		CHECK(a == 1 + 3 + 5 + 7 + 9 + 11 + 13);
-
-		// Test Range task set
-		a = 0;
-		context.range(
-			4, 1, 15, 2, [&](int i)
-			{ a += i; });
-		CHECK(a == 1 + 3 + 5 + 7 + 9 + 11 + 13);
+		for (auto taskCount : TASK_COUNT_SET)
+		{
+			testEachTaskCount(context, taskCount);
+			testRangeTaskCount(context, taskCount);
+		}
 
 		// Stop threads
 		context.stop();
