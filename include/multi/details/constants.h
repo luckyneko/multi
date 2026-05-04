4fc789968aa0508330af92eecfc93fb8a976e3c5
LuckyNeko <feik@luckyneko.com>
2026-05-04 12:03:38 +1000

    Do not pragma!

--- include/multi/details/constants.h
+++ include/multi/details/constants.h
@@ -12,10 +12,7 @@
 namespace multi
 {
 #ifdef __cpp_lib_hardware_interference_size
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Winterference-size"
 	static constexpr std::size_t CACHE_LINE_SIZE = std::hardware_destructive_interference_size;
-#pragma GCC diagnostic pop
 #else
 	static constexpr std::size_t CACHE_LINE_SIZE = 64;
 #endif
