437fe76cffea9eaf681861d8f99c2fd857df9b1f
Aaron Brown <feik@luckyneko.com>
2026-05-04 11:56:45 +1000

    Fix linux build

--- include/multi/details/constants.h
+++ include/multi/details/constants.h
@@ -6,14 +6,18 @@
 #ifndef _MULTI_CONSTANTS_H_
 #define _MULTI_CONSTANTS_H_
 
+#include <cstddef>
 #include <new>
 
 namespace multi
 {
 #ifdef __cpp_lib_hardware_interference_size
-	static constexpr size_t CACHE_LINE_SIZE = std::hardware_destructive_interference_size;
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Winterference-size"
+	static constexpr std::size_t CACHE_LINE_SIZE = std::hardware_destructive_interference_size;
+#pragma GCC diagnostic pop
 #else
-	static constexpr size_t CACHE_LINE_SIZE = 64;
+	static constexpr std::size_t CACHE_LINE_SIZE = 64;
 #endif
 
 } // namespace multi
